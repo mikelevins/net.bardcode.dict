f983c4fd1aefbd8976084f5990c7a19a634a9e1f
mikel evins <mikel@evins.net>
2021-02-04 11:17:43 -0600

    export apis

--- package.lisp
+++ package.lisp
@@ -1,4 +1,9 @@
 ;;;; package.lisp
 
 (defpackage #:net.bardcode.dict
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:dict #:dict? #:immutable-dict? #:mutable-dict?
+           #:all-keys #:all-values #:contains-key? #:contains-value?
+           #:copy-dict #:empty? #:get-key #:merge-dicts
+           #:put-key #:remove-key #:select-keys #:select-complement-keys
+           #:mutable-dict #:merge-into! #:remove-key! #:set-key!))
